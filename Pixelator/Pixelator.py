bcb22cfbcedc4e450212a243698d519a45f3279e
Tatar <tanapon9011@gmail.com>
2025-06-14 19:40:00 +0700

    Modify script to generate 2x pixelated image for "Pixel Art Smoothing"

--- Pixelator/Pixelator.py
+++ Pixelator/Pixelator.py
@@ -10,14 +10,16 @@ def pixelate_image(input_path, output_path, size=(71, 95)):
     img_small.save(output_path)
     print(f"Pixelated image saved to {output_path}")
 
-def pixelate_folder(input_folder, output_folder, size=(71, 95)):
+def pixelate_folder(input_folder, output_folder, output_folder2, size=(71, 95)):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
     for filename in os.listdir(input_folder):
         if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
             input_path = os.path.join(input_folder, filename)
             output_path = os.path.join(output_folder, filename)
+            output_path2 = os.path.join(output_folder2, filename)
             pixelate_image(input_path, output_path, size)
+            pixelate_image(output_path, output_path2 , size = (142, 190))
 
 if __name__ == "__main__":
     import sys
@@ -26,4 +28,5 @@ if __name__ == "__main__":
         sys.exit(1)
     input_folder = sys.argv[1]
     output_folder = sys.argv[2]
-    pixelate_folder(input_folder, output_folder)
+    output_folder2 = sys.argv[3]
+    pixelate_folder(input_folder, output_folder, output_folder2)
